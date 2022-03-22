4f63dec1b3af60160d386e6132ea1d1f443735bc
alonso-gg <alonsogarita02@gmail.com>
2022-03-22 14:40:58 -0600

    Ajuste en encabezado

--- fileReader.cpp
+++ fileReader.cpp
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/* ITCR Campus Central Cartago
+Ingeniería en computación - Análisis de algoritmos 
+I Semestre 2022
+Caso 2
+Alonso Garita Granados
+
+Lectura del archivo de texto, **no es objeto de evaluación para el caso**
+*/
+
 string readFile(string fileName){
     ifstream fileToRead(fileName.c_str());
     string fileContent;
